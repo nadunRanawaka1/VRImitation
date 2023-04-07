23e83a22a4d26fe3f4c808bea4106b12cd7c0bc0
Nadun Ranawaka <nadun.kesara@gmail.com>
2023-04-07 17:09:18 -0400

    Made some changes to aid debugging and for rotation control

--- Assets/GTRI-Assets/Scripts/ROS/PositionController.cs
+++ Assets/GTRI-Assets/Scripts/ROS/PositionController.cs
@@ -6,6 +6,7 @@ public class PositionController : MonoBehaviour
 {
     public GameObject rightController;
     public GameObject leftController;
+    public GameObject originPrefab;
     [Tooltip("The frame to publish the pose in")]
     public Transform originFrame; //Publish the pose with respect to this Transform
 
@@ -15,11 +16,14 @@ public class PositionController : MonoBehaviour
 
     //ROS Stuff
     public string positionTopicName = "unity_position";
+    public string poseTopicName = "unity_pose";
     private PointMsg positionMsg;
+    private PoseStampedMsg poseMsg;
 
     //Debugging Stuff
 
     public GameObject visualMarker;
+    GameObject origin;
 
     // Use this for initialization
     void Start()
@@ -41,9 +45,14 @@ public class PositionController : MonoBehaviour
 
     private void SetupRos()
     {
-        ROSConnection.GetOrCreateInstance().RegisterPublisher<PointMsg>(positionTopicName);
-        InvokeRepeating("GetAndPublishPosition", 0, 5-.0f);
-        positionMsg = new PointMsg();
+        //TODO revert this to the previous
+
+        //ROSConnection.GetOrCreateInstance().RegisterPublisher<PointMsg>(positionTopicName);
+        //InvokeRepeating("GetAndPublishPosition", 0, 2.0f);
+        //positionMsg = new PointMsg();
+        ROSConnection.GetOrCreateInstance().RegisterPublisher<PoseStampedMsg>(poseTopicName);
+        InvokeRepeating("GetAndPublishPose", 0, 0.05f);
+        poseMsg = new PoseStampedMsg();
     }
 
     public void GetAndPublishPosition()
@@ -65,10 +74,42 @@ public class PositionController : MonoBehaviour
         }
     }
 
+    public void GetAndPublishPose()
+    {
+        if (controlPose)
+        {
+            Transform original = currentController.transform.parent;
+            currentController.transform.SetParent(originFrame);
+
+            Vector3 scale = new Vector3(0.2f, 0.2f, 0.2f);
+
+            Vector3 position = currentController.transform.localPosition * 0.2f;
+            Quaternion rotation = currentController.transform.rotation;
+            Matrix4x4 transform = Matrix4x4.TRS(position, rotation, Vector3.one);
+            var finalTransform = Unity2ROS * transform * Unity2ROS;
+
+            position = finalTransform.GetColumn(3);
+            rotation = finalTransform.rotation;
+
+            Utils.GetGeometryPoint(position, poseMsg.pose.position);
+            Utils.GetGeometryQuaternion(rotation, poseMsg.pose.orientation);
+            currentController.transform.SetParent(original);
+            poseMsg.header.frame_id = "camera_depth_frame";
+            PublishPoseMsg();
+
+        }
+    }
+
     private void PublishPositionMsg()
     {
         Debug.Log("Publishing Position:" + positionMsg);
         ROSConnection.GetOrCreateInstance().Publish(positionTopicName, positionMsg);
+    }
+
+    private void PublishPoseMsg()
+    {
+        Debug.Log("Publishing Pose: " + poseMsg);
+        ROSConnection.GetOrCreateInstance().Publish(poseTopicName, poseMsg);
     }
 
     public void TogglePublish()
@@ -78,10 +119,13 @@ public class PositionController : MonoBehaviour
         if (controlPose)
         {
             //renderer.material.SetColor("_Color", Color.green);
+            origin = GameObject.Instantiate(originPrefab, currentController.transform.position, currentController.transform.rotation);
+            originFrame = origin.transform;
             visualMarker.SetActive(true);
         } else
         {
             //renderer.material.SetColor("_Color", Color.red);
+            GameObject.Destroy(origin);
             visualMarker.SetActive(false);
         }
     }
